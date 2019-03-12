20ebb2b77f7e423bc368645ae77831e439280ced
leotferreira <leo.teixeira27@gmail.com>
2019-03-12 02:14:52 -0300

    Criando opercao de instanciacao

--- src/main/java/br/com/leotf/venda/VendaApplication.java
+++ src/main/java/br/com/leotf/venda/VendaApplication.java
@@ -1,13 +1,33 @@
 package br.com.leotf.venda;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.leotf.venda.domain.Categoria;
+import br.com.leotf.venda.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class VendaApplication {
+public class VendaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
