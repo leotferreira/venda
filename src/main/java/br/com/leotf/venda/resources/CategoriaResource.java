2657a6649584c4f8b961cae9ff47a483d7aafa47
leotferreira <leo.teixeira27@gmail.com>
2019-03-13 21:02:17 -0300

    Listando todas as categorias com DTO

--- src/main/java/br/com/leotf/venda/resources/CategoriaResource.java
+++ src/main/java/br/com/leotf/venda/resources/CategoriaResource.java
@@ -3,6 +3,8 @@ package br.com.leotf.venda.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.leotf.venda.domain.Categoria;
+import br.com.leotf.venda.dto.CategoriaDTO;
 import br.com.leotf.venda.services.CategoriaService;
 
 @RestController
@@ -47,10 +50,22 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 		
-		
+			
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
 		
 	}
 	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	
-	
+}
 }
--- src/main/java/br/com/leotf/venda/services/CategoriaService.java
+++ src/main/java/br/com/leotf/venda/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.leotf.venda.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.leotf.venda.domain.Categoria;
 import br.com.leotf.venda.repositories.CategoriaRepository;
+import br.com.leotf.venda.services.exceptions.DataIntegrityException;
 import br.com.leotf.venda.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -35,4 +37,28 @@ public class CategoriaService {
 		return repo.save(obj);
 
 }
+	
+	public void deleteById(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityException e) {
+			
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+			
+		}
+		
+		
+		
+	}
+	
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
+	}
+	
+	
+	
+	
 }
