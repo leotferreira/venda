02b99d578ee9a91bea7a41a18c5724566c80d52b
leotferreira <leo.teixeira27@gmail.com>
2019-03-14 03:45:33 -0300

    Inserindo um novo cliente com POST

--- src/main/java/br/com/leotf/venda/resources/ClienteResource.java
+++ src/main/java/br/com/leotf/venda/resources/ClienteResource.java
@@ -2,6 +2,7 @@ package br.com.leotf.venda.resources;
 
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.com.leotf.venda.domain.Cliente;
 import br.com.leotf.venda.dto.ClienteDTO;
+import br.com.leotf.venda.dto.ClienteNewDTO;
 import br.com.leotf.venda.services.ClienteService;
 
 @RestController
@@ -35,6 +38,16 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 		
  }
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	
+	}
 	
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
