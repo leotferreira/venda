32a8ec401bb0c4963f3f20492d69bb416b24ef75
leotferreira <leo.teixeira27@gmail.com>
2019-03-21 20:43:36 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/br/com/leotf/venda/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package br.com.leotf.venda.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.leotf.venda.domain.Cliente;
+import br.com.leotf.venda.repositories.ClienteRepository;
+import br.com.leotf.venda.security.UserSS;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if ( cli == null) {
+			throw new UsernameNotFoundException(email);
+					
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+		
+	}
+
+}
