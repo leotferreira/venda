c48c8bfa4c0a21da8bf462f7a9713d40cb5b058c
leotferreira <leo.teixeira27@gmail.com>
2019-03-21 21:22:52 -0300

    Implementando autorizacao

--- src/main/java/br/com/leotf/venda/config/SecurityConfig.java
+++ src/main/java/br/com/leotf/venda/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.leotf.venda.security.JWTAuthenticationFilter;
+import br.com.leotf.venda.security.JWTAuthorizationFilter;
 import br.com.leotf.venda.security.JWTUtil;
 
 
@@ -63,6 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
