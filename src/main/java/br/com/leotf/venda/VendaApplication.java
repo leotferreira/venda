a9a1c7cc26287f2f6322041ebaec3b6b28fc49e9
leotferreira <leo.teixeira27@gmail.com>
2019-03-23 00:24:54 -0300

    Enviando imagem via endpoint

--- src/main/java/br/com/leotf/venda/VendaApplication.java
+++ src/main/java/br/com/leotf/venda/VendaApplication.java
@@ -1,32 +1,19 @@
 package br.com.leotf.venda;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.leotf.venda.services.S3Service;
-
-
 @SpringBootApplication
 public class VendaApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
-	
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(VendaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\fotos\\ana.jpg");
-		
-	
-		
-		
+
 	}
 
 }
